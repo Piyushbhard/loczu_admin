1f93b5bff4389c478ac378c3ef775983ab15fdca
mac <mac@macs-MacBook-Pro.local>
2025-02-05 23:25:05 +0530

    wip

--- src/cacheChecker.js
+++ src/cacheChecker.js
@@ -25,7 +25,6 @@ export function cacheChecker() {
 
 const refreshCacheAndReload = () => {
   if (caches) {
-    // Service worker cache should be cleared with caches.delete()
     caches.keys().then((names) => {
       for (const name of names) {
         caches.delete(name);
